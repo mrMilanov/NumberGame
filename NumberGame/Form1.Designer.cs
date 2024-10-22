718ad1bafbd8fc73e03886209a951a95323a9d8f
mihael <mihael@DESKTOP-C6PSCD2>
2024-10-22 22:17:04 +0300

    Add project files.

---
+++ NumberGame/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace NumberGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelNumber = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(441, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(441, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Press START button and make sure to remember all five numbers!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Numbers:";
+            // 
+            // labelNumber
+            // 
+            this.labelNumber.AutoSize = true;
+            this.labelNumber.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNumber.Location = new System.Drawing.Point(84, 151);
+            this.labelNumber.Name = "labelNumber";
+            this.labelNumber.Size = new System.Drawing.Size(0, 13);
+            this.labelNumber.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Type the numbers you just saw:";
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.Location = new System.Drawing.Point(237, 197);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(120, 22);
+            this.textBox.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(363, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 45);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "CHECK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 247);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Number Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelNumber;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ NumberGame/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberGame
+{
+    public partial class Form1 : Form
+    {
+        private List<int> numbersToShow = new List<int>();
+        private int currentIndex = 0;
+        private bool isGameRunning = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isGameRunning)
+            {
+                StartGame();
+                isGameRunning = true;
+            }
+        }
+
+        private void StartGame()
+        {
+            numbersToShow.Clear();
+            GenerateNumbersToShow();
+            ShowNumbers();
+            currentIndex = 0;
+        }
+
+        private void GenerateNumbersToShow()
+        {
+            Random random = new Random();
+            for (int i = 1; i <= 5; i++)
+            {
+                numbersToShow.Add(random.Next(1, 10));
+            }
+        }
+
+        private void ShowNumbers()
+        {
+            timer1.Interval = 1000;
+            timer1.Tick += Timer_Tick;
+            timer1.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (currentIndex < numbersToShow.Count)
+            {
+                labelNumber.Visible = true;
+                labelNumber.Text = numbersToShow[currentIndex].ToString();
+                currentIndex++;
+            }
+            else
+            {
+                timer1.Stop();
+                labelNumber.Text = "Good Luck!";
+            }
+
+            Timer hideTimer = new Timer();
+            hideTimer.Interval = 500;
+            hideTimer.Tick += (s, args) => { labelNumber.Text = ""; hideTimer.Stop(); };
+            hideTimer.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string input = textBox.Text.Trim();
+            string[] inputNumbers = input.Split(' ');
+
+            if (inputNumbers.Length != numbersToShow.Count)
+            {
+                MessageBox.Show("Please enter 5 numbers with space between them!");
+                return;
+            }
+
+            bool isCorrect = true;
+            for (int i = 0; i < numbersToShow.Count; i++)
+            {
+                int guessedNumber;
+                if (!int.TryParse(inputNumbers[i], out guessedNumber))
+                {
+                    MessageBox.Show("Please enter only numbers!");
+                    return;
+                }
+
+                if (guessedNumber != numbersToShow[i])
+                {
+                    isCorrect = false;
+                    break;
+                }
+            }
+            if (isCorrect)
+            {
+                MessageBox.Show("Congratulations! You got all the numbers!");
+                this.Close();
+            }
+            else
+            {
+                string correctNumbersText = string.Join(" ", numbersToShow);
+                MessageBox.Show($"You lose! The numbers were: {correctNumbersText}");
+                this.Close();
+            }
+        }
+    }
+}
